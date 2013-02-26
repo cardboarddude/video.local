8243035031c7bc7a095507a6f9361a6e9c33100a
gswalden <gswalden@yahoo.com>
2013-02-26 16:04:23 -0700

    removed array_flat function

--- index.php
+++ index.php
@@ -1,44 +1,4 @@
 <?php
-if( !function_exists( 'array_flat' ) ) 
-{ 
-    function array_flat( $a, $s = array( ), $l = 0 ) 
-    { 
-        # check if this is an array 
-        if( !is_array( $a ) )                           return $s; 
-        
-        # go through the array values 
-        foreach( $a as $k => $v ) 
-        { 
-            # check if the contained values are arrays 
-            if( !is_array( $v ) ) 
-            { 
-                # store the value 
-                $s[ ]       = $v; 
-                
-                # move to the next node 
-                continue; 
-                
-            } 
-            
-            # increment depth level 
-            $l++; 
-            
-            # replace the content of stored values 
-            $s              = array_flat( $v, $s, $l ); 
-            
-            # decrement depth level 
-            $l--; 
-            
-        } 
-        
-        # get only unique values 
-        if( $l == 0 ) $s = array_values( array_unique( $s ) ); 
-        
-        # return stored values 
-        return $s; 
-        
-    } # end of function array_flat( ...    
-}
 
 function globistr($string = '', $mbEncoding = ''/*optional e.g.'UTF-8'*/)
 {
@@ -155,7 +115,7 @@ if (isset($_POST['search'])):
 endif;
 
 if (isset($search)):
-	$videos = glob_files($search, 'videos');
+	$videos = glob_files($search, '..\v.lv2\videos');
 endif;
 ?>
 
